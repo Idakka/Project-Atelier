06ddc96fb1ea13e384b1a559d4733fcf81cfe544
Milo Fultz <milopfultz@gmail.com>
2021-05-25 20:59:53 -0700

    Make structure for web development
    I built out the basics of the this file structure according to the
    doumentation found in learn. I've set up nodemon to run the dev
    environment at loalhost:1234 via Express. This will currently serve up
    the `client/dist` folder, which is why the test file is in there.
    
    I'm including the package-lock so we can ensure we're all using the
    correct versions of the dependencies, which I'm assuming we should make
    routine for the project.

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const pathname = path.join(__dirname, 'client', 'dist');
+app.use(express.static(pathname));
+console.log(pathname);
+
+app.listen(1234, () => console.log('Listening at http://localhost:1234 ...'));
