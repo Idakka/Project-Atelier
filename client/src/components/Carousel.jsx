0ef9ec0cda94fbdc4ae9baab28cb02b9f32368d3
Milo Fultz <milopfultz@gmail.com>
2021-05-29 13:59:41 -0700

    Add helpful comments

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,7 +4,7 @@ import Card from './Card.jsx';
 const Carousel = () => {
   return (
     <div className="carousel">
-      <p>Carousel Placeholder</p>
+      {/* This array is purely to set the stage for the iteration we'll do later */}
       {[1].map(product => (
         <Card key={product} />
       ))}
