f5272204f2942ebae30fd144c5101e744b8fc582
Milo Fultz <milopfultz@gmail.com>
2021-05-29 12:25:49 -0700

    Add App component for src

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './src/App.jsx';
+
 ReactDOM.render(
-  <h1>Hello via React!</h1>,
+  <App />,
   document.getElementById('app')
 );
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>App Placeholder</h1>
+  );
+};
+
+export default App;
