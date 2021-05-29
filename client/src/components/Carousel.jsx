56641f8cbb81e7b47c73b1e6d002e0b33fb0dbb3
Milo Fultz <milopfultz@gmail.com>
2021-05-29 13:37:50 -0700

    Create skeleton of related items component and Carousel

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Carousel = () => {
+  return (
+    <p>Carousel Placeholder</p>
+  );
+};
+
+export default Carousel;
--- client/src/components/related_items_and_comparisons.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-var RelatedItemsAndComparisons = function(props) {
-  return (
-    <h1>RelatedItemsAndComparisons!</h1>
-  );
-};
-
-export default RelatedItemsAndComparisons;
