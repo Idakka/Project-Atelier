2b3d6d5e345b340170598b4a67d40feec0db81bd
Milo Fultz <milopfultz@gmail.com>
2021-05-31 11:53:45 -0700

    Add cleanup to tests

--- client/__tests__/StarReview.test.js
+++ client/__tests__/StarReview.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import StarReview from '../src/components/StarReview';
 
+afterEach(() => {
+  cleanup();
+});
+
 test('Should render StarReview component', () => {
   render(<StarReview rating={3} />);
   const StarReviewComponent = screen.getByTestId('star-review');
