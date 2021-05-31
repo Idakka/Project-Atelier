2fc8cab8dbc3bde1510011d7243bd77c90d260bc
Milo Fultz <milopfultz@gmail.com>
2021-05-31 11:18:47 -0700

    Flesh out star component

--- client/src/components/StarReview.jsx
+++ client/src/components/StarReview.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const StarReview = ({ rating }) => (
-  <h1>STAR REVIEW: {rating}</h1>
-);
+const StarReview = ({ rating }) => {
+  const stars = [];
+
+  const totalStars = 5;
+  // Add a full star for every whole number
+  for (let i = 0; i < Math.floor(rating); i++) {
+    stars.push(<div className="star-review__star star-review__star--full" key={i}><img src="#" alt="Full star" /></div>);
+  }
+  // If decimal between .5 and 1, show half-star graphic
+  if ((rating - stars.length) >= .5) {
+    stars.push(<div className="star-review__star star-review__star--half" key={1}><img src="#" alt="Half star" /></div>);
+  }
+  // Fill the rest with empty stars
+  for (let i = stars.length; i < totalStars; i++) {
+    stars.push(<div className="star-review__star star-review__star--empty" key={i}><img src="#" alt="Empty star" /></div>);
+  }
+
+  return <div className="star-review">{stars}</div>;
+};
 
 export default StarReview;
