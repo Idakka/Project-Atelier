ba49819a25886987d6587756e638b62a8667755a
Sam Brown <baudot@gmail.com>
2021-06-02 08:36:29 -0700

    Fixed bug: flexbox doesn't respect width, needs min-width.

--- client/src/components/SalesPitch.jsx
+++ client/src/components/SalesPitch.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 
 var SalesPitch = function(props) {
   return (
-    <span>SalesPitch!</span>
+    <div className="overview-column-container">
+      <div id="sales-pitch-paragraph">
+        <h3>Product Slogan. Pithy Description Or Catchphrase</h3>
+        <p>
+          Cupcake ipsum dolor sit amet lemon drops gummies chupa chups. Croissant chocolate cake gummi bears. Soufflé cheesecake jelly beans ice cream sweet. Bonbon tootsie roll candy apple pie powder chocolate sesame snaps cookie. Chocolate candy canes soufflé bear claw jelly sesame snaps chocolate bar dragée. Liquorice powder chupa chups icing sesame snaps caramels chocolate cake. Powder fruitcake powder cake marshmallow.
+        </p><p>
+          Brownie halvah lollipop cupcake dessert. Croissant bonbon sweet cookie cake. Toffee macaroon powder tart jelly beans caramels.
+        </p>
+      </div>
+      <div className="vertical-rule"></div>
+      <div className="sales-pitch-checklist">
+        <div><span className="material-icons">done</span>Totally Sweet.</div>
+        <div><span className="material-icons">done</span>Totally Sweet.</div>
+        <div><span className="material-icons">done</span>Totally Sweet.</div>
+        <div><span className="material-icons">done</span>Totally Sweet.</div>
+      </div>
+    </div>
   );
 };
 
