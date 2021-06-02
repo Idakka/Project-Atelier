dd6c6dfe7164f90543176c71a8424d967cb67482
Milo Fultz <milopfultz@gmail.com>
2021-06-02 07:00:00 -0700

    Implement dotenv for environment variables
    For making API calls, we will need to be using our Github token, which
    should be secure and not checked into the repo. dotenv lets you store
    whatever in a `.env` file that can be recalled with `process.env.NAME`,
    with NAME being the key of a `KEY=VALUE` pair within the `.env` file.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const path = require('path');
+const dotenv = require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 1234;
+
+// console.log(process.env.GITHUB_TOKEN);
 
 const pathname = path.join(__dirname, 'public');
 app.use(express.static(pathname));
