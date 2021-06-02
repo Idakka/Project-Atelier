3caee93a1da8569d7ab4fece7aa8adc58ca5d856
Milo Fultz <milopfultz@gmail.com>
2021-06-02 07:55:11 -0700

    Add get related items route

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const dotenv = require('dotenv').config();
 const axios = require('axios');
 const path = require('path');
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const port = 1234;
+axios.defaults.headers.common['Authorization'] = process.env.GITHUB_TOKEN;
 
 const pathname = path.join(__dirname, 'public');
 app.use(express.static(pathname));
 
 app.get('/', (req, res) => {
   res.send('hello world from server!');
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.product_id}/related`)
+    .then(response => res.end(JSON.stringify(response.data)))
+    .catch(err => res.end(JSON.stringify(err)));
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
