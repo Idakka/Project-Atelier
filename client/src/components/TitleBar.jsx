9f6d8828a6acf5174a919282fd59e5774db7a8b2
Sam Brown <baudot@gmail.com>
2021-06-01 17:55:19 -0700

    Title bar component, styling.

--- client/src/components/TitleBar.jsx
+++ client/src/components/TitleBar.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 var TitleBar = function(props) {
   return (
     <div className="title-bar">
-      <span className="title">Logo</span>
-      <input type="text" default="________" id="title_bar_search" />
-      <span className="material-icons-outlined">search</span>
-
+      <div className="logo">Logo</div>
+      <div className="titlebar-search">
+        <input type="text" default="________" id="title_bar_search" />
+        <span className="material-icons-outlined">search</span>
+      </div>
     </div>
   );
 };
