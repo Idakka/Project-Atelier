7ae58588cfafc4adc09abf6c89586d2d5b0df619
Sam Brown <baudot@gmail.com>
2021-06-01 19:28:50 -0700

    Dummy text copied over to SitewideAnnounce.jsx.

--- client/src/components/SiteWideAnnounce.jsx
+++ client/src/components/SiteWideAnnounce.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 var SiteWideAnnounce = function(props) {
   return (
-    <span>SiteWideAnnounce!</span>
+    <div className="sitewide-announce">
+      SITE-WIDE ANNOUNCEMENT MESSAGE! &mdash; SALE / DISCOUNT <strong>OFFER</strong> &mdash; <u>NEW PRODUCT HIGHLIGHT</u>
+    </div>
   );
 };
 
