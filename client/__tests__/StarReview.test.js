562e7564c92e48b81535f8f1f657d372d1371b26
Milo Fultz <milopfultz@gmail.com>
2021-06-01 18:42:06 -0700

    Add quarter star functionality

--- client/__tests__/StarReview.test.js
+++ client/__tests__/StarReview.test.js
@@ -39,15 +39,26 @@ test('Should render 1 full star, 1 half star and 3 empty stars on a 1.5', () => 
   expect(numberOfEmptyStars.length).toBe(3);
 });
 
-test('Should render 1 full star, 1 half star and 3 empty stars on a 1.9', () => {
+test('Should render 1 full star, 1 three quarter star and 3 empty stars on a 1.9', () => {
   render(<StarReview rating={1.9} />);
   const StarReviewComponent = screen.getByTestId('star-review');
   const numberOfFullStars = screen.getAllByAltText('Full star');
-  const numberOfHalfStars = screen.getAllByAltText('Half star');
+  const numberOfHalfStars = screen.getAllByAltText('Three quarter star');
   const numberOfEmptyStars = screen.getAllByAltText('Empty star');
   expect(numberOfFullStars.length).toBe(1);
   expect(numberOfHalfStars.length).toBe(1);
   expect(numberOfEmptyStars.length).toBe(3);
+});
+
+test('Should render 1 full star, 3 empty stars and 1 quarter star on a 1.4', () => {
+  render(<StarReview rating={1.4} />);
+  const StarReviewComponent = screen.getByTestId('star-review');
+  const numberOfFullStars = screen.getAllByAltText('Full star');
+  const numberOfEmptyStars = screen.getAllByAltText('Empty star');
+  const numberOfQuarterStars = screen.getAllByAltText('Quarter star');
+  expect(numberOfFullStars.length).toBe(1);
+  expect(numberOfEmptyStars.length).toBe(3);
+  expect(numberOfQuarterStars.length).toBe(1);
 });
 
 test('Should render 1 full star, 0 half stars and 4 empty stars on a 1.2', () => {
--- client/src/components/StarReview.jsx
+++ client/src/components/StarReview.jsx
@@ -8,9 +8,17 @@ const StarReview = ({ rating }) => {
   for (let i = 0; i < Math.floor(rating); i++) {
     stars.push(<div className="star-review__star star-review__star--full" key={i}><img src="#" alt="Full star" /></div>);
   }
-  // If decimal between .5 and 1, show half-star graphic
+  // If decimal between .75 and 1, show half-star graphic
+  if ((rating - stars.length) >= .75) {
+    stars.push(<div className="star-review__star star-review__star--threequarter" key={1}><img src="#" alt="Three quarter star" /></div>);
+  }
+  // If decimal between .5 and .75, show half-star graphic
   if ((rating - stars.length) >= .5) {
     stars.push(<div className="star-review__star star-review__star--half" key={1}><img src="#" alt="Half star" /></div>);
+  }
+  // If decimal between .25 and .5, show 1/4-star graphic
+  if ((rating - stars.length) >= .25) {
+    stars.push(<div className="star-review__star star-review__star--quarter" key={1}><img src="#" alt="Quarter star" /></div>);
   }
   // Fill the rest with empty stars
   for (let i = stars.length; i < totalStars; i++) {
