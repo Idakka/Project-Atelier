fa77822888f9c6e95141c93cc56878240b5efb29
Milo Fultz <milopfultz@gmail.com>
2021-06-03 08:02:57 -0700

    Get product with API, use dummy data for styles/reviews

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,44 +1,88 @@
 import React, { useEffect, useState } from 'react';
 import StarReview from './StarReview.jsx';
 
-const Card = ({ productId, cardType }) => {
-  // All of these are default values that we will remove when API implementation is ready
-  const [photoURL, setPhotoURL] = React.useState('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png');
-  const [category, setCategory] = React.useState('Placeholder Category');
-  const [name, setName] = React.useState('Placeholder Name');
-  const [price, setPrice] = React.useState('$100.00');
-  const [rating, setRating] = React.useState(4);
+const Card = ({ product, cardType }) => {
+  const [styles, setStyles] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
-  const starGraphic = '#';
-  const removeGraphic = '#';
-
-  React.useEffect(() => {
-    // make API call to get product info. in cb:
-      // set category
-      // set name
-      // set price
-    // make API call to get rating info. in cb:
-      // set rating
-    // make API call to get style info. in cb:
-      // set image url
+  useEffect(() => {
+    setStyles([
+      {
+        'style_id': 1,
+        'name': 'Forest Green & Black',
+        'original_price': '140',
+        'sale_price': '0',
+        'default?': true,
+        'photos': [
+          {
+            'thumbnail_url': 'urlplaceholder/style_1_photo_number_thumbnail.jpg',
+            'url': 'urlplaceholder/style_1_photo_number.jpg'
+          },
+          {
+            'thumbnail_url': 'urlplaceholder/style_1_photo_number_thumbnail.jpg',
+            'url': 'urlplaceholder/style_1_photo_number.jpg'
+          }
+        ],
+        'skus': {},
+      }
+    ]);
+    setReviews([
+      {
+        'review_id': 5,
+        'rating': 3,
+        'summary': 'I\'m enjoying wearing these shades',
+        'recommend': false,
+        'response': null,
+        'body': 'Comfortable and practical.',
+        'date': '2019-04-14T00:00:00.000Z',
+        'reviewer_name': 'shortandsweeet',
+        'helpfulness': 5,
+        'photos': [
+          {
+            'id': 1,
+            'url': 'urlplaceholder/review_5_photo_number_1.jpg'
+          },
+          {
+            'id': 2,
+            'url': 'urlplaceholder/review_5_photo_number_2.jpg'
+          },
+        ]
+      },
+      {
+        'review_id': 3,
+        'rating': 4,
+        'summary': 'I am liking these glasses',
+        'recommend': false,
+        'response': 'Glad you\re enjoying the product!',
+        'body': 'They are very dark. But that\'s good because I\'m in very sunny spots',
+        'date': '2019-06-23T00:00:00.000Z',
+        'reviewer_name': 'bigbrotherbenjamin',
+        'helpfulness': 5,
+        'photos': [],
+      }
+    ]);
   }, []);
+
+  console.log(styles);
+
+  cardType = 'related';
 
   return (
     <div className="card product">
       <div className="product__img">
-        <img src={photoURL} alt={name} />
+        <img src={styles.length !== 0 ? styles[0].photos[0].url : '#'} alt={product.name} />
         {cardType === 'related' && (
-          <img src={starGraphic} alt="Star" className="card__action" />
+          <span className="material-icons">star_outline</span>
         )}
         {cardType === 'outfit' && (
-          <img src={removeGraphic} alt="Remove" className="card__action" />
+          <span className="material-icons">do_not_disturb</span>
         )}
       </div>
       <div className="product__details">
-        <p className="product__category">{category}</p>
-        <p className="product__name">{name}</p>
-        <p className="product__price">{price}</p>
-        <StarReview rating={rating} />
+        <p className="product__category">{product.category}</p>
+        <p className="product__name">{product.name}</p>
+        <p className="product__price">{product.price}</p>
+        <StarReview rating={reviews.length !== 0 ? reviews[0].rating : 0} />
       </div>
     </div>
   );
--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Card from './Card.jsx';
 import axios from 'axios';
 
-const Carousel = ({ productId }) => {
+const Carousel = ({ productId, cardType }) => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
+    // if cardType is related
     axios.get('/products/22122/related')
       .then(response => {
         console.log(response.data);
-        return setProducts(response.data);
+        setProducts(response.data);
       })
       .catch(err => console.log(err));
+    // if cardType is outfit
   }, []);
 
   return (
     <div className="carousel">
       {products.map(product => (
-        <Card key={product.id} />
+        <Card key={product.id} product={product} cardType={cardType} />
       ))}
     </div>
   );
