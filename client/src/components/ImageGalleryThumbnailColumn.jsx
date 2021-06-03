a3aed2dc7d734ad2c14a8f32c76d863f7f41d996
Sam Brown <baudot@gmail.com>
2021-06-03 14:01:26 -0700

    First draft of the overview section is complete!

--- client/src/components/ImageGalleryThumbnailColumn.jsx
+++ client/src/components/ImageGalleryThumbnailColumn.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ImageGalleryThumbnail from './ImageGalleryThumbnail.jsx';
+import ImageGalleryThumbnailColumnUpArrow from './ImageGalleryThumbnailColumnUpArrow.jsx';
+import ImageGalleryThumbnailColumnDownArrow from './ImageGalleryThumbnailColumnDownArrow.jsx';
 
 var ImageGalleryThumbnailColumn = function(props) {
   return (
     <div className="image-gallery-thumbnail-column">
+      <ImageGalleryThumbnailColumnUpArrow />
       <ImageGalleryThumbnail selected={true}/>
       <ImageGalleryThumbnail />
       <ImageGalleryThumbnail />
+      <ImageGalleryThumbnailColumnDownArrow />
     </div>
   );
 };
---
+++ client/src/components/ImageGalleryThumbnailColumnDownArrow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var ImageGalleryThumbnailColumnDownArrow = function() {
+  return (
+    <div className="image-gallery-thumbnail-column-down-arrow">
+      <span className="material-icons">expand_more</span>
+    </div>
+  );
+};
+
+export default ImageGalleryThumbnailColumnDownArrow;
