dec8a3110a0574af553f9e6aa67f7052ecb74148
Sam Brown <baudot@gmail.com>
2021-06-02 17:21:41 -0700

    WIP: Product Detail Right elements started

--- client/src/components/ProductInformation.jsx
+++ client/src/components/ProductInformation.jsx
@@ -5,9 +5,9 @@ var ProductInformation = function(props) {
     <React.Fragment>
       <div>Star Review</div>
       <div>
-        Category
-        Expanded Product Name
-        $Price
+        <div className="product-detail-category">Category</div>
+        <div className="product-detail-expanded-product-name">Expanded Product Name</div>
+        <div className="product-detail-price">$Price</div>
       </div>
     </React.Fragment>
   );
