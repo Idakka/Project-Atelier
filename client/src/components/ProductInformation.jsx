150bdb99713b34d9f391fd15673890fc09bdefc1
Sam Brown <baudot@gmail.com>
2021-06-02 19:34:55 -0700

    Styling on ProductInformation.jsx starting to look pretty decent.

--- client/src/components/ProductInformation.jsx
+++ client/src/components/ProductInformation.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import StarReview from './StarReview.jsx';
 
 var ProductInformation = function(props) {
   return (
     <React.Fragment>
-      <div>Star Review</div>
+      <StarReview rating='4.5'/><span className="read-all-reviews">Read all reviews</span>
       <div>
         <div className="product-detail-category">Category</div>
         <div className="product-detail-expanded-product-name">Expanded Product Name</div>
-        <div className="product-detail-price">$Price</div>
+        <div className="product-detail-price">$369</div>
       </div>
     </React.Fragment>
   );
