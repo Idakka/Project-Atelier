9372c4b414294e28240e33c595f360d49011719b
Sam Brown <baudot@gmail.com>
2021-06-04 14:55:25 -0700

    Just a bunch of "Component X successfully renders" tests for the Overview widget

--- client/src/components/StyleSelector.jsx
+++ client/src/components/StyleSelector.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var StyleSelector = function(props) {
   return (
     <React.Fragment>
-      <div className="style-selector-header">
+      <div className="style-selector-header" data-testid='style-selector'>
         <span className="style-label">STYLE &gt;</span> SELECTED STYLE
       </div>
       <div>
