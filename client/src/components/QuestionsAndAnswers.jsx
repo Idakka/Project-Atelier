d5125349d1c21d988f9d594e81e930c0bde9141b
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-04 16:37:31 -0700

    Fixes style change requested in review

--- client/src/components/QuestionsAndAnswers.jsx
+++ client/src/components/QuestionsAndAnswers.jsx
@@ -32,11 +32,11 @@ const QuestionsAndAnswers = () => {
             <h3>[Product Name]: [Question Body]</h3>
             <form className="add-answer-form">
               <label className="add-form-answer">Answer*</label>
-              <textarea rows="10" maxLength="1000" required></textarea><p></p>
+              <textarea rows="10" maxLength="1000" required /><p></p>
               <label className="add-form-username">Nickname*</label>
-              <input maxLength="60" placeholder="Example: jack543!" required></input><p className="form-small">For privacy reasons, do not use your full name or email address</p>
+              <input maxLength="60" placeholder="Example: jack543!" required /><p className="form-small">For privacy reasons, do not use your full name or email address</p>
               <label className="add-form-email">Email*</label>
-              <input maxLength="60" placeholder="Example: jack@email.com" required></input><p className="form-small">For authentication reasons, you will not be emailed</p>
+              <input maxLength="60" placeholder="Example: jack@email.com" required /><p className="form-small">For authentication reasons, you will not be emailed</p>
               <button className="add-form-submit" onClick={() => setModalIsOpenAdd(false)}>Submit</button>
             </form>
           </Modal>
@@ -69,11 +69,11 @@ const QuestionsAndAnswers = () => {
         <h3>About the [Product Name Placeholder]</h3>
         <form className="add-question-form">
           <label className="add-form-question">Question*</label>
-            <textarea rows="10" maxLength="1000" required></textarea><p></p>
+            <textarea rows="10" maxLength="1000" required /><p></p>
           <label className="add-form-username">Nickame*</label>
-            <input maxLength="60" placeholder="Example: jackson11!" required></input><p className="form-small">For privacy reasons, do not use your full name or email address</p>
+            <input maxLength="60" placeholder="Example: jackson11!" required /><p className="form-small">For privacy reasons, do not use your full name or email address</p>
           <label className="add-form-email">Email*</label>
-            <input maxLength="60" required></input><p className="form-small">For authentication reasons, you will not be emailed</p>
+            <input maxLength="60" required /><p className="form-small">For authentication reasons, you will not be emailed</p>
           <button className="add-form-submit" onClick={() => setModalIsOpen(false)}>Submit</button>
         </form>
       </Modal>
