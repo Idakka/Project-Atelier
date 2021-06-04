1fd43761bc226f773eb9208801df6e953a24dc68
Sam Brown <baudot@gmail.com>
2021-06-03 17:40:27 -0700

    Putting in Milo's recommendations.

--- client/src/components/VerticalRule.jsx
+++ client/src/components/VerticalRule.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 var VerticalRule = function(props) {
   return (
-    <div className="vertrule-containter">
+    <div className="vertrule-container">
       <div className="vertical-rule"></div>
     </div>
   );
