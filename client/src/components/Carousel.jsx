503dd706bd7cd5bd97a3d23343e3ef0cb63f5d8d
Milo Fultz <milopfultz@gmail.com>
2021-06-04 18:51:05 -0700

    Add initial load calibration of scroll

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -6,9 +6,26 @@ const Carousel = ({ productId, carouselType }) => {
   const [products, setProducts] = useState([]);
   const [scrollPosition, setScrollPosition] = useState(0);
   const [canScrollLeft, setCanScrollLeft] = useState(false);
-  const [canScrollRight, setCanScrollRight] = useState(true);
+  const [canScrollRight, setCanScrollRight] = useState(false);
 
   const slideWrapperRef = createRef();
+
+  const scrollCarousel = useCallback((change, containerWidth, numberOfProducts = products.length) => {
+    // Get boundary variables for scroll calibration
+    const cardWidth = 272;
+    const capacity = Math.floor(containerWidth / cardWidth);
+    const farLeft = 0;
+    const farRight = Math.max(numberOfProducts - capacity, 0);
+
+    // If you can't scroll in a direction, remove the button
+    setCanScrollLeft(scrollPosition + change !== farLeft);
+    setCanScrollRight(scrollPosition + change !== farRight);
+
+    // If you are not at the edge, then scroll one more card over
+    if ((change === -1 && scrollPosition !== farLeft) || (change === 1 && scrollPosition !== farRight)) {
+      setScrollPosition(scrollPosition + change);
+    }
+  });
 
   useEffect(() => {
     // if carouselType is related
@@ -16,31 +33,14 @@ const Carousel = ({ productId, carouselType }) => {
       .then(response => {
         console.log(response.data);
         setProducts(response.data);
+        // Have to pass in window width as first render has not occurred and number of products since setProducts is async
+        // qqq Add an OR clause for when container has maxwidth
+        scrollCarousel(0, window.innerWidth, response.data.length);
       })
       .catch(err => console.log(err));
     // if carouselType is outfit
       // get all products saved in outfit and setProducts
   }, []);
-
-  const scrollCarousel = useCallback((direction) => {
-    // Get boundary variables for scroll calibration
-    const cardWidth = 272;
-    const capacity = Math.floor(slideWrapperRef.current.clientWidth / cardWidth);
-    const farLeft = 0;
-    const farRight = products.length - capacity;
-    const change = direction === 'left' ? -1 : 1;
-
-    // If you can't scroll in a direction, remove the button
-    setCanScrollLeft(scrollPosition + change !== farLeft);
-    setCanScrollRight(scrollPosition + change !== farRight);
-
-    // If you are not at the edge, then scroll one more card over
-    if (direction === 'left' && scrollPosition !== farLeft) {
-      setScrollPosition(scrollPosition - 1);
-    } else if (direction === 'right' && scrollPosition !== farRight) {
-      setScrollPosition(scrollPosition + 1);
-    }
-  });
 
   return (
     <React.Fragment>
@@ -50,7 +50,12 @@ const Carousel = ({ productId, carouselType }) => {
           className="carousel__edge carousel__edge--left"
           style={{visibility: canScrollLeft ? 'visible' : 'hidden', opacity: canScrollLeft ? 1 : 0}}
         >
-          <span className="material-icons" onClick={() => scrollCarousel('left')}>arrow_backward</span>
+          <span
+            className="material-icons"
+            onClick={() => scrollCarousel(-1, slideWrapperRef.current.clientWidth)}
+          >
+            arrow_backward
+          </span>
         </div>
         <div ref={slideWrapperRef} className="carousel__slide-wrapper">
           <div className="carousel" style={{ left: String(scrollPosition * -272) + 'px' }}>
@@ -63,7 +68,12 @@ const Carousel = ({ productId, carouselType }) => {
           className="carousel__edge carousel__edge--right"
           style={{visibility: canScrollRight ? 'visible' : 'hidden', opacity: canScrollRight ? 1 : 0}}
         >
-          <span className="material-icons" onClick={() => scrollCarousel('right')}>arrow_forward</span>
+          <span
+            className="material-icons"
+            onClick={() => scrollCarousel(1, slideWrapperRef.current.clientWidth)}
+          >
+            arrow_forward
+          </span>
         </div>
       </div>
     </React.Fragment>
