62fbb93d8131483d9c4253e946a11dda5dfddaa3
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-06-08 23:34:25 -0500

    fixed spacing issue

--- client/src/mockData/cartMock.js
+++ client/src/mockData/cartMock.js
@@ -1,15 +1,15 @@
 const cartMock = [
   {
-      "sku_id": 1,
-      "count": 2
+    'sku_id': 1,
+    'count': 2
   },
   {
-      "sku_id": 3,
-      "count": 1
+    'sku_id': 3,
+    'count': 1
   },
   {
-      "sku_id": 5,
-      "count": 33
+    'sku_id': 5,
+    'count': 33
   },
 ];
 
