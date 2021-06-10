8ab98ce33489d857a0eee483c2d3b78d61d4fd9b
Milo Fultz <milopfultz@gmail.com>
2021-06-10 07:06:22 -0700

    Clean up mock data

--- client/src/mockData/productInfoMock.js
+++ client/src/mockData/productInfoMock.js
@@ -1,19 +1,23 @@
 const productInfoMock = {
-  'id':22126,
-  'campus':'hr-rpp',
-  'name':'Heir Force Ones',
-  'slogan':'A sneaker dynasty',
-  'description':'Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I\'m just a sneaker pro, I love Pumas and shell toes, but can\'t nothin compare to a fresh crispy white pearl',
-  'category':'Kicks',
-  'default_price':'99.00',
-  'created_at':'2021-03-18T16:09:30.589Z','updated_at':'2021-03-18T16:09:30.589Z',
-  'features':[
-    {'feature':'Sole','value':'Rubber'},
-    {'feature':'Material',
-      'value':'FullControlSkin'},
-    {'feature':'Mid-Sole',
-      'value':'ControlSupport Arch Bridge'},
-    {'feature':'Stitching','value':'Double Stitch'}
+  'id': 22126,
+  'campus': 'hr-rpp',
+  'name': 'Heir Force Ones',
+  'slogan': 'A sneaker dynasty',
+  'description': 'Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I\'m just a sneaker pro, I love Pumas and shell toes, but can\'t nothin compare to a fresh crispy white pearl',
+  'category': 'Kicks',
+  'default_price': '99.00',
+  'created_at': '2021-03-18T16:09:30.589Z', 'updated_at': '2021-03-18T16:09:30.589Z',
+  'features': [
+    { 'feature': 'Sole', 'value': 'Rubber' },
+    {
+      'feature': 'Material',
+      'value': 'FullControlSkin'
+    },
+    {
+      'feature': 'Mid-Sole',
+      'value': 'ControlSupport Arch Bridge'
+    },
+    { 'feature': 'Stitching', 'value': 'Double Stitch' }
   ]
 };
 
@@ -431,59 +435,88 @@ const productStylesMock =
 
 const productsMock = [
   {
-    'id':22122,
-    'campus':'hr-rpp',
-    'name':'Camo Onesie',
-    'slogan':'Blend in to your crowd',
-    'description':'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
-    'category':'Jackets',
-    'default_price':'140.00',
-    'created_at':'2021-03-18T16:09:30.589Z',
-    'updated_at':'2021-03-18T16:09:30.589Z'
+    'id': 22122,
+    'campus': 'hr-rpp',
+    'name': 'Camo Onesie',
+    'slogan': 'Blend in to your crowd',
+    'description': 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+    'category': 'Jackets',
+    'default_price': '140.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
   },
   {
-    'id':22123,'campus':'hr-rpp',
-    'name':'Bright Future Sunglasses',
-    'slogan':'You\'ve got to wear shades',
-    'description':'Where you\'re going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.','category':'Accessories',
-    'default_price':'69.00',
-    'created_at':'2021-03-18T16:09:30.589Z',
-    'updated_at':'2021-03-18T16:09:30.589Z'
+    'id': 22123, 'campus': 'hr-rpp',
+    'name': 'Bright Future Sunglasses',
+    'slogan': 'You\'ve got to wear shades',
+    'description': 'Where you\'re going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.', 'category': 'Accessories',
+    'default_price': '69.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
   },
   {
-    'id':22124,'campus':'hr-rpp',
-    'name':'Morning Joggers',
-    'slogan':'Make yourself a morning person',
-    'description':'Whether you\'re a morning person or not.  Whether you\'re gym bound or not.  Everyone looks good in joggers.',
-    'category':'Pants',
-    'default_price':'40.00',
-    'created_at':'2021-03-18T16:09:30.589Z',
-    'updated_at':'2021-03-18T16:09:30.589Z'},
-  {'id':22125,
-    'campus':'hr-rpp',
-    'name':'Slacker\'s Slacks',
-    'slogan':'Comfortable for everything, or nothing',
-    'description':'I\'ll tell you how great they are after I nap for a bit.',
-    'category':'Pants',
-    'default_price':'65.00',
-    'created_at':'2021-03-18T16:09:30.589Z',
-    'updated_at':'2021-03-18T16:09:30.589Z'
+    'id': 22124, 'campus': 'hr-rpp',
+    'name': 'Morning Joggers',
+    'slogan': 'Make yourself a morning person',
+    'description': 'Whether you\'re a morning person or not.  Whether you\'re gym bound or not.  Everyone looks good in joggers.',
+    'category': 'Pants',
+    'default_price': '40.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
   },
   {
-    'id':22126,
-    'campus':'hr-rpp',
-    'name':'Heir Force Ones',
-    'slogan':'A sneaker dynasty',
-    'description':'Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I\'m just a sneaker pro, I love Pumas and shell toes, but can\'t nothin compare to a fresh crispy white pearl',
-    'category':'Kicks',
-    'default_price':'99.00',
-    'created_at':'2021-03-18T16:09:30.589Z','updated_at':'2021-03-18T16:09:30.589Z'
+    'id': 22125,
+    'campus': 'hr-rpp',
+    'name': 'Slacker\'s Slacks',
+    'slogan': 'Comfortable for everything, or nothing',
+    'description': 'I\'ll tell you how great they are after I nap for a bit.',
+    'category': 'Pants',
+    'default_price': '65.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
   },
   {
-    'id':22127,'campus':'hr-rpp','name':'Pumped Up Kicks','slogan':'Faster than a just about anything','description':'The Pumped Up serves up crisp court style with a modern look. These shoes show off tennis-whites shades and are constructed with a supple leather upper and a classic rubber cupsole.','category':'Kicks','default_price':'89.00','created_at':'2021-03-18T16:09:30.589Z','updated_at':'2021-03-18T16:09:30.589Z'
+    'id': 22126,
+    'campus': 'hr-rpp',
+    'name': 'Heir Force Ones',
+    'slogan': 'A sneaker dynasty',
+    'description': 'Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I\'m just a sneaker pro, I love Pumas and shell toes, but can\'t nothin compare to a fresh crispy white pearl',
+    'category': 'Kicks',
+    'default_price': '99.00',
+    'created_at': '2021-03-18T16:09:30.589Z', 'updated_at': '2021-03-18T16:09:30.589Z'
   },
   {
-    'id':22128,'campus':'hr-rpp','name':'Blues Suede Shoes','slogan':'2019 Stanley Cup Limited Edition','description':'Touch down in the land of the Delta Blues in the middle of the pouring rain','category':'Dress Shoes','default_price':'120.00','created_at':'2021-03-18T16:09:30.589Z','updated_at':'2021-03-18T16:09:30.589Z'},{'id':22129,'campus':'hr-rpp','name':'YEasy 350','slogan':'Just jumped over jumpman','description':'These stretchy knit shoes show off asymmetrical lacing and a big sculpted rubber midsole. In a nod to adidas soccer heritage.','category':'Kicks','default_price':'450.00','created_at':'2021-03-18T16:09:30.589Z','updated_at':'2021-03-18T16:09:30.589Z'
+    'id': 22127,
+    'campus': 'hr-rpp',
+    'name': 'Pumped Up Kicks',
+    'slogan': 'Faster than a just about anything',
+    'description': 'The Pumped Up serves up crisp court style with a modern look. These shoes show off tennis-whites shades and are constructed with a supple leather upper and a classic rubber cupsole.',
+    'category': 'Kicks',
+    'default_price': '89.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
+  },
+  {
+    'id': 22128,
+    'campus': 'hr-rpp',
+    'name': 'Blues Suede Shoes',
+    'slogan': '2019 Stanley Cup Limited Edition',
+    'description': 'Touch down in the land of the Delta Blues in the middle of the pouring rain',
+    'category': 'Dress Shoes',
+    'default_price': '120.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
+  },
+  {
+    'id': 22129,
+    'campus': 'hr-rpp',
+    'name': 'YEasy 350',
+    'slogan': 'Just jumped over jumpman',
+    'description': 'These stretchy knit shoes show off asymmetrical lacing and a big sculpted rubber midsole. In a nod to adidas soccer heritage.',
+    'category': 'Kicks',
+    'default_price': '450.00',
+    'created_at': '2021-03-18T16:09:30.589Z',
+    'updated_at': '2021-03-18T16:09:30.589Z'
   }
 ];
 
--- client/src/mockData/reviewsMock.js
+++ client/src/mockData/reviewsMock.js
@@ -1,37 +1,129 @@
 const reviewsMock = {
-  "product":"22122",
-  "page":0,"count":8,
-  "results":[
+  'product': '22122',
+  'page': 0,
+  'count': 8,
+  'results': [
     {
-      "review_id":406680,
-      "rating":3,
-      "summary":"Djembe",
-      "recommend":true,
-      "response":null,
-      "body":"Djembe",
-      "date":"2021-06-09T00:00:00.000Z",
-      "reviewer_name":"Djembe",
-      "helpfulness":0,
-      "photos":[]
+      'review_id': 406680,
+      'rating': 3,
+      'summary': 'Djembe',
+      'recommend': true,
+      'response': null,
+      'body': 'Djembe',
+      'date': '2021-06-09T00:00:00.000Z',
+      'reviewer_name': 'Djembe',
+      'helpfulness': 0,
+      'photos': []
     },
     {
-      "review_id":406679,"rating":3,"summary":"Djembe","recommend":true,"response":null,"body":"Djembe","date":"2021-06-09T00:00:00.000Z","reviewer_name":"Djembe","helpfulness":0,"photos":[]},{"review_id":406678,"rating":3,"summary":"Djembe","recommend":true,"response":null,"body":"Djembe","date":"2021-06-09T00:00:00.000Z","reviewer_name":"Djembe","helpfulness":0,"photos":[]},{"review_id":308845,"rating":2,"summary":"my review","recommend":true,"response":null,"body":"is thus to be revered like this","date":"2021-03-30T00:00:00.000Z","reviewer_name":"Arf Barley","helpfulness":0,"photos":[{"id":536090,"url":"http://www.unsplash.com/3"}]},{"review_id":308844,"rating":2,"summary":"my review","recommend":true,"response":null,"body":"is thus to be revered like this","date":"2021-03-30T00:00:00.000Z","reviewer_name":"Arf Barley","helpfulness":0,"photos":[{"id":536089,"url":"http://www.unsplash.com/3"}]},{"review_id":289140,"rating":4,"summary":"This product was ok!","recommend":false,"response":"","body":"I really did not like this product solely because I am tiny and do not fit into it.","date":"2019-01-11T00:00:00.000Z","reviewer_name":"mymainstreammother","helpfulness":2,"photos":[]},{"review_id":289139,"rating":5,"summary":"This product was great!","recommend":true,"response":"","body":"I really did or did not like this product based on whether it was sustainably sourced. Then I found out that its made from nothing at all.","date":"2019-01-01T00:00:00.000Z","reviewer_name":"funtime","helpfulness":8,"photos":[]
+      'review_id': 406679,
+      'rating': 3,
+      'summary': 'Djembe',
+      'recommend': true,
+      'response': null,
+      'body': 'Djembe',
+      'date': '2021-06-09T00:00:00.000Z',
+      'reviewer_name': 'Djembe',
+      'helpfulness': 0,
+      'photos': []
+    }, {
+      'review_id': 406678,
+      'rating': 3,
+      'summary': 'Djembe',
+      'recommend': true,
+      'response': null,
+      'body': 'Djembe',
+      'date': '2021-06-09T00:00:00.000Z',
+      'reviewer_name': 'Djembe',
+      'helpfulness': 0,
+      'photos': []
+    },
+    {
+      'review_id': 308845,
+      'rating': 2,
+      'summary': 'my review',
+      'recommend': true,
+      'response': null,
+      'body': 'is thus to be revered like this',
+      'date': '2021-03-30T00:00:00.000Z',
+      'reviewer_name': 'Arf Barley',
+      'helpfulness': 0,
+      'photos': [{
+        'id': 536090,
+        'url': 'http://www.unsplash.com/3'
+      }]
+    },
+    {
+      'review_id': 308844,
+      'rating': 2,
+      'summary': 'my review',
+      'recommend': true,
+      'response': null,
+      'body': 'is thus to be revered like this',
+      'date': '2021-03-30T00:00:00.000Z',
+      'reviewer_name': 'Arf Barley',
+      'helpfulness': 0,
+      'photos': [{
+        'id': 536089,
+        'url': 'http://www.unsplash.com/3'
+      }]
+    },
+    {
+      'review_id': 289140,
+      'rating': 4,
+      'summary': 'This product was ok!',
+      'recommend': false,
+      'response': '',
+      'body': 'I really did not like this product solely because I am tiny and do not fit into it.',
+      'date': '2019-01-11T00:00:00.000Z',
+      'reviewer_name': 'mymainstreammother',
+      'helpfulness': 2,
+      'photos': []
+    },
+    {
+      'review_id': 289139,
+      'rating': 5,
+      'summary': 'This product was great!',
+      'recommend': true,
+      'response': '',
+      'body': 'I really did or did not like this product based on whether it was sustainably sourced. Then I found out that its made from nothing at all.',
+      'date': '2019-01-01T00:00:00.000Z',
+      'reviewer_name': 'funtime',
+      'helpfulness': 8,
+      'photos': []
     }
   ]
 };
 
 const reviewsMetaMock = {
-  "product_id":"22122",
-  "ratings":{"2":"2","3":"3","4":"1","5":"1"},
-  "recommended":{"false":"1","true":"6"},
-  "characteristics":{
-    "Fit":{
-      "id":74277,
-      "value":"4.6190476190476190"
+  'product_id': '22122',
+  'ratings': {
+    '2': '2',
+    '3': '3',
+    '4': '1',
+    '5': '1'
+  },
+  'recommended': {
+    'false': '1',
+    'true': '6'
+  },
+  'characteristics': {
+    'Fit': {
+      'id': 74277,
+      'value': '4.6190476190476190'
     },
-    "Length":{"id":74278,"value":"3.5000000000000000"},
-    "Comfort":{"id":74279,"value":"5.0000000000000000"},
-    "Quality":{"id":74280,"value":"4.0000000000000000"}
+    'Length': {
+      'id': 74278,
+      'value': '3.5000000000000000'
+    },
+    'Comfort': {
+      'id': 74279,
+      'value': '5.0000000000000000'
+    },
+    'Quality': {
+      'id': 74280,
+      'value': '4.0000000000000000'
+    }
   }
 };
 
