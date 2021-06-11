5ba67f0d09e181ac25e50f657e6c29d5dc5e0b21
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-11 16:56:25 -0700

    Creates QA folder within client

--- client/__tests__/QA.test.js
+++ client/__tests__/QA.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Modal from 'react-modal';
 import { render, screen, cleanup } from '@testing-library/react';
-import QuestionsAndAnswers from '../src/components/QuestionsAndAnswers';
-import QACardAnswers from '../src/components/QACardAnswers';
-import QACardQuestions from '../src/components/QACardQuestions';
+import QuestionsAndAnswers from '../src/components/QA/QuestionsAndAnswers';
+import QACardAnswers from '../src/components/QA/QACardAnswers';
+import QACardQuestions from '../src/components/QA/QACardQuestions';
 
 afterEach(cleanup);
 
