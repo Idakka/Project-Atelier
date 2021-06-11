8e3140edd114c5fc6affe0b51edada63d45e1335
Sam Brown <baudot@gmail.com>
2021-06-11 13:38:32 -0700

    Sales pitch area now being generated from the mock data.

--- client/src/components/SalesPitch.jsx
+++ client/src/components/SalesPitch.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import VerticalRule from './VerticalRule.jsx';
 
 var SalesPitch = function(props) {
+  let { productInfo } = props;
+  let { description, slogan, features } = productInfo;
+  let checklistItems = features.map((feature) => {
+    let innerText = feature.feature + ': ' + feature.value;
+    return <div key={feature.feature}><span className="material-icons">done</span>{innerText}</div>;
+  });
   return (
     <div className="overview-column-container">
       <div id="sales-pitch-paragraph">
-        <h3>Product Slogan. Pithy Description Or Catchphrase</h3>
+        <h3>{slogan}</h3>
         <p>
-          Cupcake ipsum dolor sit amet lemon drops gummies chupa chups. Croissant chocolate cake gummi bears. Soufflé cheesecake jelly beans ice cream sweet. Bonbon tootsie roll candy apple pie powder chocolate sesame snaps cookie. Chocolate candy canes soufflé bear claw jelly sesame snaps chocolate bar dragée. Liquorice powder chupa chups icing sesame snaps caramels chocolate cake. Powder fruitcake powder cake marshmallow.
-        </p><p>
-          Brownie halvah lollipop cupcake dessert. Croissant bonbon sweet cookie cake. Toffee macaroon powder tart jelly beans caramels.
+          {description}
         </p>
       </div>
       <VerticalRule />
       <div className="sales-pitch-checklist">
-        <div><span className="material-icons">done</span>Totally Sweet.</div>
-        <div><span className="material-icons">done</span>Totally Sweet.</div>
-        <div><span className="material-icons">done</span>Totally Sweet.</div>
-        <div><span className="material-icons">done</span>Totally Sweet.</div>
+        {checklistItems}
       </div>
     </div>
   );
 };
 
 export default SalesPitch;
+
+//         <div><span className="material-icons">done</span>Totally Sweet.</div>
