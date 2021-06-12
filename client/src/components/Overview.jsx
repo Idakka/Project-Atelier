4d58070d1564673c703946c68baf2b61ca24d858
Sam Brown <baudot@gmail.com>
2021-06-11 19:29:10 -0700

    Star rating now computes from review metadata, in the Overview, anyway.

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -15,7 +15,7 @@ var Overview = function(props) {
       <div className="overview-column-container">
         <ImageGallery />
         <div className="product-information">
-          <ProductInformation productInfo={props.productInfo}/>
+          <ProductInformation productInfo={props.productInfo} reviewsMeta={props.reviewsMeta} />
           <StyleSelector />
           <AddToCartRegion top={props.top} />
         </div>
--- client/src/components/ProductInformation.jsx
+++ client/src/components/ProductInformation.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import StarReview from './StarReview.jsx';
 
+const calculateRating = function(ratings) {
+  let ratingCount = 0;
+  let ratingTally = 0;
+  for (let i = 1; i <= 5; i++) {
+    if (ratings[i]) {
+      console.log(i + ": " + ratings[i]);
+      ratingCount += Number.parseInt(ratings[i]);
+      ratingTally += Number.parseInt(ratings[i]) * i;
+    }
+  }
+  return ratingTally / parseFloat(ratingCount);
+};
+
 var ProductInformation = function(props) {
-  let { productInfo } = props;
+  let { productInfo, reviewsMeta } = props;
   let { default_price, category, name } = productInfo;
+  let { ratings } = reviewsMeta;
+  console.log('ratings: ', ratings);
   return (
     <React.Fragment>
       <div className="stars-and-reviews">
-        <StarReview rating='4.5'/>&nbsp;<span className="read-all-reviews">Read all reviews</span>
+        <StarReview rating={calculateRating(ratings)}/>&nbsp;<span className="read-all-reviews">Read all reviews</span>
       </div>
       <div data-testid="product-information">
         <div className="product-detail-category">{category}</div>
