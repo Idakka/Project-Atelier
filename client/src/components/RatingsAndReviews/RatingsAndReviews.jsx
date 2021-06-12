85952bf393c1f2972243bd886ef5cddee58e5d85
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-06-12 02:49:44 -0500

    basic reviews list hiding and show more button

--- client/src/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ client/src/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StarReview from '../StarReview.jsx';
 import ReviewsList from './ReviewsList.jsx';
 
 const RatingsAndReviews = function({ onChangeFileHandler, onClickUploadHandler, productId, reviews, reviewsMeta }) {
+
+  const [_reviews, setReviews] = useState([]);
+  const [reviewsLength, setReviewsLength] = useState(0);
+  const [reviewsToShow, setReviewsToShow] = useState([]);
+  const [reviewsToShowLength, setReviewsToShowLength] = useState(2);
+
+  useEffect(() => {
+    setReviews(reviews);
+    setReviewsLength(reviews.length);
+    setReviewsToShow(reviews.slice(0, reviewsToShowLength));
+    setReviewsToShowLength(reviewsToShowLength + 2);
+  }, []);
+
   return (
     <div id="ratings-main" data-testid="ratings-main">
       <div id="ratings-left-pane">
@@ -21,7 +34,7 @@ const RatingsAndReviews = function({ onChangeFileHandler, onClickUploadHandler, 
       <div id="ratings-right-pane">
         <div id="ratings-title">{reviews.length} reviews, sorted by
 
-          <select name="sort-parameter" id="sort-parameter" onChange={(i) => {alert("This feature only available to premium subscribers!")}}>
+          <select name="sort-parameter" id="sort-parameter" onChange={(i) => {alert("This feature only available to premium subscribers!");} }>
             <option value="relevance">relevance
 
             </option>
@@ -30,10 +43,13 @@ const RatingsAndReviews = function({ onChangeFileHandler, onClickUploadHandler, 
 
           </select>
         </div>
-        <ReviewsList reviews={reviews} />
+        <ReviewsList reviews={reviewsToShow} />
         <div id="ratings-right-pane-footer">
-          <div className="review-btn">
-            <button onClick={(i) => {alert("This feature only available to premium subscribers!")}}>MORE REVIEWS</button>
+          <div type="button" className="review-btn">
+            <button className="rnr-button" onClick={() => {
+        setReviewsToShowLength(reviewsToShowLength + 2);
+        setReviewsToShow(_reviews.slice(0, reviewsToShowLength));
+      }}>MORE REVIEWS</button>
           </div>
           <div className="review-add">
             <input type="file" name="file" onChange={onChangeFileHandler} />
