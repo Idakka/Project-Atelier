d6e7d46032d1dbbf31ad056e36a0d6712e0d88d1
NylonEric <51034223+NylonEric@users.noreply.github.com>
2021-06-11 21:36:13 -0500

    added review date with formatting using date-fns package

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import StarReview from './StarReview.jsx';
+import { format } from "date-fns";
 
-const Review = (props) => {
+const Review = ({review}) => {
   return (
     <React.Fragment>
       <div className="review-card">
         <div className="review-header">
-          <StarReview rating={props.review.rating}/>
-          <div className="author">User1234, January 1, 2019</div>
+          <StarReview rating={review.rating}/>
+          <div className="author">{review.reviewer_name}, {format((new Date(review.date)), "MMMM do, yyyy")}
+          </div>
         </div>
         <div className="review-title">Review Title with word-break...</div>
         <div className="review-block">
-              here is some dynamic text: {props.review.summary}
+              here is some dynamic text: {review.summary}
         </div>
         <div className="response-block">Response:</div>
         <div className="review-footer">Helpful? Yes (9) | Report
