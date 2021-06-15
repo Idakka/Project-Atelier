0077dd02ea36f6d4c7c4c08abcce254231e7c483
Milo Fultz <milopfultz@gmail.com>
2021-06-14 20:15:11 -0700

    Convert RelatedItemsAndOutfit to functional with props

--- client/src/components/RelatedItemsAndOutfit/RelatedItemsAndOutfit.jsx
+++ client/src/components/RelatedItemsAndOutfit/RelatedItemsAndOutfit.jsx
@@ -1,48 +1,36 @@
 import React from 'react';
 import Carousel from './Carousel.jsx';
 
-class RelatedItemsAndOutfit extends React.Component {
-  // when using top level state, this will be a functional component that takes in
-  // all details about the products via props
-
-  constructor (props) {
-    super(props);
-    this.state = {
-      productInfo: this.props.productInfo,
-      productStyles: this.props.productStyles,
-      relatedProducts: this.props.relatedProducts,
-      reviewsMeta: this.props.reviewsMeta
-    };
-  }
-
-  render() {
-    return (
-      <section
-        id="related-items-and-outfit"
-        className="related-items-and-outfit"
-        data-testid="related-items-and-outfit"
-      >
-        <div id="related-items">
-          <Carousel
-            product={this.state.productInfo}
-            styles={this.state.productStyles}
-            related={this.state.relatedProducts}
-            reviews={this.state.reviewsMeta}
-            carouselType={'related'}
-          />
-        </div>
-        <div id="your-outfit">
-          <Carousel
-            product={this.state.productInfo}
-            styles={this.state.productStyles}
-            related={this.state.relatedProducts}
-            reviews={this.state.reviewsMeta}
-            carouselType={'outfit'}
-          />
-        </div>
-      </section>
-    );
-  }
-}
+const RelatedItemsAndOutfit = ({ productInfo, productStyles, relatedProducts, reviewsMeta }) => {
+  return (
+    <section
+      id="related-items-and-outfit"
+      className="related-items-and-outfit"
+      data-testid="related-items-and-outfit"
+    >
+      <div id="related-items">
+        {/* The Carousel will need to refactor its inputs to accommodate the correct set of data */}
+        {/* It will take in a list of objects from top level state, e.g. iterate through related */}
+        {/* and populate an array of those objects that the Carousel/Card can then use. */}
+        <Carousel
+          product={productInfo}
+          styles={productStyles}
+          related={relatedProducts}
+          reviews={reviewsMeta}
+          carouselType={'related'}
+        />
+      </div>
+      <div id="your-outfit">
+        <Carousel
+          product={productInfo}
+          styles={productStyles}
+          related={relatedProducts}
+          reviews={reviewsMeta}
+          carouselType={'outfit'}
+        />
+      </div>
+    </section>
+  );
+};
 
 export default RelatedItemsAndOutfit;
