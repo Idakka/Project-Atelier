c2ff2b063e6ea38b90476e3e8cee8b70091b9393
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-16 00:29:08 -0700

    Initial search questions functionality is working

--- client/src/components/QA/QACardQuestions.jsx
+++ client/src/components/QA/QACardQuestions.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal';
-import axios from 'axios';
-import QuestionsAndAnswers from './QuestionsAndAnswers.jsx';
+import React, { useState, useEffect } from 'react';
 import QACardAnswers from './QACardAnswers.jsx';
 import QAAddAnswerModal from './QAAddAnswerModal.jsx';
+import QAAddQuestionModal from './QAAddQuestionModal.jsx';
 
 const QACardQuestions = ({ questions, productName }) => {
 
@@ -11,6 +9,12 @@ const QACardQuestions = ({ questions, productName }) => {
   // NOTE - keeping for refactor later on based on server changes for this path
   // // answers array for current product
   // const [answers, setAnswers] = useState([]);
+  const [word, setWord] = useState('');
+  // questions array for current product
+  const [questionLength, setQuestionLength] = useState(questions.length);
+  // array for questions to display
+  const [questionsToShowLength, setQuestionsToShowLength] = useState(2);
+  const [questionsToShow, setQuestionsToShow] = useState(questions.slice(0, questionsToShowLength));
 
   // useEffect(() => {
   //   axios.get('/qa/questions/:question_id/answers')
@@ -20,17 +24,48 @@ const QACardQuestions = ({ questions, productName }) => {
   //     .catch(err => err);
   // }, []);
 
+  const increaseCount = () => {
+    event.preventDefault();
+    setQuestionsToShowLength(questionsToShow + 2);
+    setQuestionsToShow(questions.slice(0, questionsToShowLength));
+  };
+
+
   return (
-    <div data-testid="qa-questions">
-      <div>{questions.map((question, index) =>
-        <div className="qa-card-sample" key={index}>
-          <b><div className="qa-div">Q: {question.question_body}
-            <div className="qa-helpfulness-right" data-testid="qa-helpfulness-right">Helpful? <a href='' className='right-spacing'>Yes ({question.question_helpfulness}) </a>
-              <QAAddAnswerModal question={question} productName={productName} />
-            </div>
-          </div></b>
+    <div>
+      <form data-testid="search" id="search">
+        <input className="qa-searchbar" data-testid="qa-searchbar" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
+          onChange={() => {
+            if (event.target.value.length < 3) {
+              setWord('');
+            }
+            if (event.target.value.length >= 3) {
+              setWord(event.target.value);
+            }
+            console.log('word', word, word.length);
+          }}
+        ></input>
+        {/* NOTE - Search button will be replaced with icon later on */}
+        <button className="qa-search-btn" onClick={() => {
+        }}>Search</button>
+      </form>
+      {questions.filter((item) => item.question_body.includes(word)).map((question, index) => (
+        <div key={index}>
+          <div className="qa-card-sample" key={index}>
+            <b><div className="qa-div">Q: {question.question_body}
+              <div className="qa-helpfulness-right" data-testid="qa-helpfulness-right">Helpful? <a href='' className='right-spacing'>Yes ({question.question_helpfulness}) </a>
+                <QAAddAnswerModal question={question} productName={productName} />
+              </div>
+            </div></b>
+          </div>
           <div data-testid="answers"><QACardAnswers currentAnswers={question.answers} /></div>
-        </div>)}</div>
+        </div>
+      ))}
+      <div className="qa-footer-buttons" data-testid="qa-footer-buttons">
+        <button className="qa-more" onClick={() => {
+        }}>MORE ANSWERED QUESTIONS</button>
+        <div className="qa-more" data-testid="qa-more"><QAAddQuestionModal productName={productName} /></div>
+      </div>
     </div>
   );
 };
--- client/src/components/QA/QuestionsAndAnswers.jsx
+++ client/src/components/QA/QuestionsAndAnswers.jsx
@@ -2,20 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import axios from 'axios';
 import QACardQuestions from './QACardQuestions.jsx';
-import QACardAnswers from './QACardAnswers.jsx';
-import QAAddQuestionModal from './QAAddQuestionModal.jsx';
-// import { questionsMock } from '../../mockData/questionsMock.js';
 
 const QuestionsAndAnswers = (props) => {
-
   // const [modalIsOpen, setModalIsOpen] = useState(false);
   const [product, setProduct] = useState('');
-  // questions array for current product
+  // // questions array for current product
   const [questions, setQuestions] = useState([]);
-  const [questionLength, setQuestionLength] = useState(0);
-  // array for questions to display
-  const [questionsToShow, setQuestionsToShow] = useState([]);
-  const [questionsToShowLength, setQuestionsToShowLength] = useState(4);
   // variables to take in product info
   const [productName, setProductName] = useState('');
 
@@ -24,9 +16,6 @@ const QuestionsAndAnswers = (props) => {
     axios.get('/qa/questions/')
       .then(response => {
         setQuestions(response.data);
-        setQuestionLength(response.data.length);
-        setQuestionsToShow(response.data.slice(0, questionsToShowLength));
-        setQuestionsToShowLength(questionsToShowLength + 2);
       })
       .catch(err => err);
   }, []);
@@ -39,23 +28,7 @@ const QuestionsAndAnswers = (props) => {
   return (
     <div data-testid="qa-div" id="qa">
       <h1 id="qa-header">QUESTIONS AND ANSWERS</h1>
-
-      <form data-testid="search" id="search">
-        <input className="qa-searchbar" data-testid="qa-searchbar" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."></input>
-        {/* NOTE - Search button will be replaced with icon later on  */}
-        <button className="qa-search-btn">Search</button>
-      </form>
-
-      <div data-testid="qa" id="qa"><QACardQuestions questions={questionsToShow} productName={productName}/></div>
-
-      <div className="qa-footer-buttons" data-testid="qa-footer-buttons">
-        <button className="qa-more" onClick={() => {
-          setQuestionsToShowLength(questionsToShowLength + 2);
-          setQuestionsToShow(questions.slice(0, questionsToShowLength));
-        }}>MORE ANSWERED QUESTIONS</button>
-        <div className="qa-more" data-testid="qa-more"><QAAddQuestionModal productName={productName}/></div>
-      </div>
-
+      <QACardQuestions questions={questions} productName={productName}/>
       <p></p>
     </div>
 
