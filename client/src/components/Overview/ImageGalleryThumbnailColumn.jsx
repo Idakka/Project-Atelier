7d97248206b76c8ac832e3de1ee67d399ae80ebf
Sam Brown <baudot@gmail.com>
2021-06-19 08:52:15 -0700

    Well.... that's not how the thumbnails are meant to move... but it's progress of a kind.

--- client/src/components/Overview/ImageGalleryThumbnailColumn.jsx
+++ client/src/components/Overview/ImageGalleryThumbnailColumn.jsx
@@ -3,9 +3,29 @@ import ImageGalleryThumbnail from './ImageGalleryThumbnail.jsx';
 import ImageGalleryThumbnailColumnUpArrow from './ImageGalleryThumbnailColumnUpArrow.jsx';
 import ImageGalleryThumbnailColumnDownArrow from './ImageGalleryThumbnailColumnDownArrow.jsx';
 
+const IGTC_THUMBNAIL_HEIGHT = 75;
+
 class ImageGalleryThumbnailColumn extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { thumbnailPosition: 0 }
+  }
+
+  componentDidMount() {
+    document.getElementById('ig-thumbnail-column-up-arrow').addEventListener('click', (event) => {
+      let currentThumbnailPosition = this.state.thumbnailPosition;
+      console.log('Up arrow clicked. Current Thumbnail position: ', currentThumbnailPosition);
+      if (currentThumbnailPosition < this.props.productStyles.results[0].photos.length - 1) {
+        this.setState({thumbnailPosition: currentThumbnailPosition + 1});
+      }
+    });
+    document.getElementById('ig-thumbnail-column-down-arrow').addEventListener('click', (event) => {
+      let currentThumbnailPosition = this.state.thumbnailPosition;
+      console.log('Down arrow clicked. Current Thumbnail position: ', currentThumbnailPosition);
+      if (currentThumbnailPosition > 0) {
+        this.setState({thumbnailPosition: currentThumbnailPosition - 1});
+      }
+    });
   }
 
   render() {
@@ -16,7 +36,7 @@ class ImageGalleryThumbnailColumn extends React.Component {
     return (
       <div className="ig-thumbnail-column" data-testid="ig-tc">
         <ImageGalleryThumbnailColumnUpArrow />
-          <div className="ig-thumbnail-inner-container">
+          <div className="ig-thumbnail-inner-container" style={{ top: IGTC_THUMBNAIL_HEIGHT * -this.state.thumbnailPosition + "px" }}>
             {thumbnails}
           </div>
         <ImageGalleryThumbnailColumnDownArrow />
