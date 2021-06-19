87535b5534dd43d9d8eafdcdbe896c5b6128f6f6
Sam Brown <baudot@gmail.com>
2021-06-18 20:59:20 -0700

    Limit thumbnails to height of image gallery.

--- client/src/components/Overview/ImageGalleryThumbnailColumn.jsx
+++ client/src/components/Overview/ImageGalleryThumbnailColumn.jsx
@@ -4,15 +4,16 @@ import ImageGalleryThumbnailColumnUpArrow from './ImageGalleryThumbnailColumnUpA
 import ImageGalleryThumbnailColumnDownArrow from './ImageGalleryThumbnailColumnDownArrow.jsx';
 
 var ImageGalleryThumbnailColumn = function({ productStyles }) {
-  console.log('productStyles in thumbnail column: ', productStyles);
   let thumbnails = productStyles.results[0].photos.map((style) => {
     return <ImageGalleryThumbnail key={style.thumbnail_url} thumbnail_url={style.thumbnail_url} />
   });
 
   return (
-    <div className="image-gallery-thumbnail-column" data-testid="ig-tc">
+    <div className="ig-thumbnail-column" data-testid="ig-tc">
       <ImageGalleryThumbnailColumnUpArrow />
-      {thumbnails}
+        <div className="ig-thumbnail-inner-container">
+          {thumbnails}
+        </div>
       <ImageGalleryThumbnailColumnDownArrow />
     </div>
   );
