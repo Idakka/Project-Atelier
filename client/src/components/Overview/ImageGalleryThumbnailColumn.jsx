b2b6330f76aefdf9496e9d4ca4c64822769ec27a
Sam Brown <baudot@gmail.com>
2021-06-19 08:10:18 -0700

    Making IG Thumbnail gallery classful in anticipation of adding a componentDidMount to attach button handlers events.

--- client/src/components/Overview/ImageGalleryThumbnailColumn.jsx
+++ client/src/components/Overview/ImageGalleryThumbnailColumn.jsx
@@ -3,20 +3,26 @@ import ImageGalleryThumbnail from './ImageGalleryThumbnail.jsx';
 import ImageGalleryThumbnailColumnUpArrow from './ImageGalleryThumbnailColumnUpArrow.jsx';
 import ImageGalleryThumbnailColumnDownArrow from './ImageGalleryThumbnailColumnDownArrow.jsx';
 
-var ImageGalleryThumbnailColumn = function({ productStyles }) {
-  let thumbnails = productStyles.results[0].photos.map((style) => {
-    return <ImageGalleryThumbnail key={style.thumbnail_url} thumbnail_url={style.thumbnail_url} />
-  });
+class ImageGalleryThumbnailColumn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div className="ig-thumbnail-column" data-testid="ig-tc">
-      <ImageGalleryThumbnailColumnUpArrow />
-        <div className="ig-thumbnail-inner-container">
-          {thumbnails}
-        </div>
-      <ImageGalleryThumbnailColumnDownArrow />
-    </div>
-  );
+  render() {
+    let thumbnails = this.props.productStyles.results[0].photos.map((style) => {
+      return <ImageGalleryThumbnail key={style.thumbnail_url} thumbnail_url={style.thumbnail_url} />
+    });
+
+    return (
+      <div className="ig-thumbnail-column" data-testid="ig-tc">
+        <ImageGalleryThumbnailColumnUpArrow />
+          <div className="ig-thumbnail-inner-container">
+            {thumbnails}
+          </div>
+        <ImageGalleryThumbnailColumnDownArrow />
+      </div>
+    );
+  }
 };
 
 export default ImageGalleryThumbnailColumn;
