9e6404a3949eb158f3fa84bd5f181deed8676b3b
Sam Brown <baudot@gmail.com>
2021-06-21 19:53:48 -0700

    Minor tweaks before branching off the branch.

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -3,21 +3,33 @@ import ImageGalleryThumbnailColumn from './ImageGalleryThumbnailColumn.jsx';
 import ImageGalleryForwardArrow from './ImageGalleryForwardArrow.jsx';
 import ImageGalleryBackArrow from './ImageGalleryBackArrow.jsx';
 
-var ImageGallery = function({ productStyles }) {
-  let photo = productStyles.results[0].photos[0].url;
-  return (
-    <div className="image-gallery-container" data-testid="image-gallery">
-      <div className="image-gallery">
-        <img className="ig-main-image" src={photo} />
-        <ImageGalleryBackArrow />
-        <ImageGalleryForwardArrow />
-        <ImageGalleryThumbnailColumn productStyles={productStyles} />
-        <div className="image-gallery-fullscreen-toggle">
-          <span className="material-icons">fullscreen</span>
+class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStyle: 0, // this will later move up to the overview component, but for quick testing, it's here.
+      selectedPhoto: 0
+    }
+  };
+
+  render() {
+    let { selectedStyle, selectedPhoto } = this.state;
+    let { productStyles } = this.props;
+    let photo = productStyles.results[selectedStyle].photos[selectedPhoto].url;
+    return (
+      <div className="image-gallery-container" data-testid="image-gallery">
+        <div className="image-gallery">
+          <img className="ig-main-image" src={photo} />
+          <ImageGalleryBackArrow />
+          <ImageGalleryForwardArrow />
+          <ImageGalleryThumbnailColumn productStyles={productStyles} />
+          <div className="image-gallery-fullscreen-toggle">
+            <span className="material-icons">fullscreen</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default ImageGallery;
