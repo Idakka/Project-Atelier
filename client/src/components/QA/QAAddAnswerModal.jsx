bf158a18672908866bf88e89726e869447cd4c82
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-22 22:55:58 -0700

    Refactors Add Answer Modal

--- client/src/components/QA/QAAddAnswerModal.jsx
+++ client/src/components/QA/QAAddAnswerModal.jsx
@@ -21,7 +21,7 @@ const QAAddAnswerModal = ({ question, productName }) => {
 
   return (
     <div data-testid="qa-div-card-questions">
-      <button onClick={() => setModalIsOpenAdd(true)} className="qa-add-answer" href=""><b>[Add Answer]</b></button>
+      {/* <button onClick={() => setModalIsOpenAdd(true)} className="qa-add-answer" href=""><b>[Add Answer]</b></button>
       <Modal isOpen={modalIsOpenAdd} onRequestClose={() => setModalIsOpenAdd(false)}>
         <h2>Submit Your Answer</h2>
         <h3>{productName.name}: {question.question_body} </h3>
@@ -42,7 +42,40 @@ const QAAddAnswerModal = ({ question, productName }) => {
             });
           }}>Submit</button>
         </form>
-      </Modal>
+      </Modal> */}
+
+      <button id="qa-modal-button" className="qa-add" onClick={() => {
+        document.getElementById("qa-modal-answers").style.display = "block";
+      }}><b>[Add Answer]</b></button>
+
+      <div id="qa-modal-answers" className="qa-modal">
+        <div className="qa-modal-content">
+          <div className="qa-modal-close" onClick={() => {
+            document.getElementById("qa-modal-answers").style.display = "none";
+          }}>&times;</div>
+          <h2>Submit Your Answer</h2>
+            <h3>{productName.name}: {question.question_body} </h3>
+            <form className="add-answer-form">
+              <label className="add-form-answer">Answer* </label>
+                <textarea rows="10" maxLength="1000" required onChange={() => setAnswer(event.target.value)}/><p></p>
+              <label className="add-form-username">Nickname*</label>
+                <input maxLength="60" placeholder="Example: jack543!" required onChange={() => setNickname(event.target.value)}/><p className="qa-form-small">For privacy reasons, do not use your full name or email address</p>
+              <label className="add-form-email">Email*</label>
+                <input maxLength="60" placeholder="Example: jack@email.com" required onChange={() => setEmail(event.target.value)}/><p className="qa-form-small">For authentication reasons, you will not be emailed</p>
+              <button className="qa-add-form-submit" onClick={() => {
+                event.preventDefault();
+                setModalInfo({
+                  answer: answer,
+                  nickname: nickname,
+                  email: email
+                });
+              }}>Submit</button>
+            </form>
+        </div>
+      </div>
+
+
+
     </div>
   );
 };
