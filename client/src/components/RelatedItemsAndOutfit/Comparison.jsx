9b38c61388f276b0c4fc97edfbb05779480791d4
Milo Fultz <milopfultz@gmail.com>
2021-06-23 18:35:32 -0700

    Make HTML skeleton for comparison modal

--- client/src/components/RelatedItemsAndOutfit/Comparison.jsx
+++ client/src/components/RelatedItemsAndOutfit/Comparison.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const Comparison = ({ top, currentProduct, comparisonProduct }) => {
+const Comparison = ({ top, currentProduct, relatedProduct }) => {
   return (
     <React.Fragment>
-      <h1>Example Modal Window</h1>
+      <h3>Comparing</h3>
+      <div className="comparison__product-titles">
+        <h2>Current Product Name</h2>
+        <h2>Related Product Name</h2>
+      </div>
+      <div className="comparison-characteristics">
+        {/* For each characteristic up to X amount */}
+        <div className="comparison-characteristics__row">
+          <div className="comparison-characteristics__current">
+            {true && (
+              <span className="material-icons check">check</span>
+            )}
+          </div>
+          <div className="comparison-characteristics__characteristic">Product Characteristic</div>
+          <div className="comparison-characteristics__related">
+            {true && (
+              <span className="material-icons check">check</span>
+            )}
+          </div>
+        </div>
+      </div>
       <button
         id='default-modal-close-button'
         data-testid="example-modal-close-button"
