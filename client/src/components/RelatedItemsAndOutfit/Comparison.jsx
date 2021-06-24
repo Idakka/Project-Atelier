d727da01a44919cf8698a0677e46d46697b46160
Milo Fultz <milopfultz@gmail.com>
2021-06-23 19:28:47 -0700

    Style the skeleton

--- client/src/components/RelatedItemsAndOutfit/Comparison.jsx
+++ client/src/components/RelatedItemsAndOutfit/Comparison.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 
 const Comparison = ({ top, currentProduct, relatedProduct }) => {
   return (
-    <React.Fragment>
-      <h3>Comparing</h3>
+    <div className="comparison__wrapper">
+      <h3 className="comparison__title">Comparing</h3>
       <div className="comparison__product-titles">
-        <h2>Current Product Name</h2>
-        <h2>Related Product Name</h2>
+        <h3 className="comparison__product-title">Current Product Name</h3>
+        <h3 className="comparison__product-title">Related Product Name</h3>
       </div>
       <div className="comparison-characteristics">
-        {/* For each characteristic up to X amount */}
-        <div className="comparison-characteristics__row">
-          <div className="comparison-characteristics__current">
-            {true && (
-              <span className="material-icons check">check</span>
-            )}
+        {[1,2,3].map(trait => (
+          <div className="comparison-characteristics__row">
+            <div className="comparison-characteristics__current">
+              {/* If current has the trait, add the check */}
+              {true && (
+                <span className="material-icons check">check</span>
+              )}
+            </div>
+            <div className="comparison-characteristics__characteristic">Product Characteristic</div>
+            <div className="comparison-characteristics__related">
+              {/* If related has the trait, add the check */}
+              {true && (
+                <span className="material-icons check">check</span>
+              )}
+            </div>
           </div>
-          <div className="comparison-characteristics__characteristic">Product Characteristic</div>
-          <div className="comparison-characteristics__related">
-            {true && (
-              <span className="material-icons check">check</span>
-            )}
-          </div>
-        </div>
+        ))}
       </div>
       <button
         id='default-modal-close-button'
@@ -31,7 +34,7 @@ const Comparison = ({ top, currentProduct, relatedProduct }) => {
       >
         Click to hide modal.
       </button>
-    </React.Fragment>
+    </div>
   );
 };
 
