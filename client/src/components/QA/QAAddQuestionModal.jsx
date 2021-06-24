30a1c5377f58ff00f2a672bd07091f2bebab7567
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-23 19:38:17 -0700

    Work in progress

--- client/src/components/QA/QAAddQuestionModal.jsx
+++ client/src/components/QA/QAAddQuestionModal.jsx
@@ -15,14 +15,14 @@ const QAAddQuestionModal = ({ productName }) => {
           <h2>Ask Your Question</h2>
           <h3>About the {productName.name}</h3>
           <form className="add-answer-form">
-           <label className="add-form-question">Question*</label>
-           <textarea rows="10" maxLength="1000" required /><p></p>
-           <label className="add-form-username">Nickame*</label>
-           <input maxLength="60" placeholder="Example: jackson11!" required /><p className="qa-form-small">For privacy reasons, do not use your full name or email address</p>
-           <label className="add-form-email">Email*</label>
-           <input maxLength="60" required /><p className="qa-form-small">For authentication reasons, you will not be emailed</p>
-             <button className="qa-add-form-submit" >Submit</button>
-           </form>
+            <label className="add-form-question">Question*</label>
+            <textarea rows="10" maxLength="1000" required /><p></p>
+            <label className="add-form-username">Nickame*</label>
+            <input maxLength="60" placeholder="Example: jackson11!" required /><p className="qa-form-small">For privacy reasons, do not use your full name or email address</p>
+            <label className="add-form-email">Email*</label>
+            <input maxLength="60" required /><p className="qa-form-small">For authentication reasons, you will not be emailed</p>
+            <button className="qa-add-form-submit" >Submit</button>
+          </form>
         </div>
       </div>
     </div>
