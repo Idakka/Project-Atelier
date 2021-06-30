888587b549dd57a5bcc70d03a607561c167c0f51
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-29 23:09:18 -0700

    Load/collapse working again

--- client/src/components/QA/QACardAnswers.jsx
+++ client/src/components/QA/QACardAnswers.jsx
@@ -30,41 +30,101 @@ const QACardAnswers = ({ currentAnswers }) => {
     setToggle(true);
   }, []);
 
-  const APICall = (answerId) => {
-    axios.put(`/qa/answers/:answer_id/helpful`, {answer_id: answerId})
+  const APICallHelpful = (answerId) => {
+    axios.put(`/qa/answers/:answer_id/helpful`, { answer_id: answerId })
       .then(info => console.log('info:', info))
       .catch(err => err);
-  }
+  };
 
   const APICallReport = (answerId) => {
-    axios.put(`/qa/answers/:answer_id/report`, {answer_id: answerId})
+    axios.put(`/qa/answers/:answer_id/report`, { answer_id: answerId })
       .then(info => console.log('info:', info))
       .catch(err => err);
+  };
+
+  // if there are no answers
+  if (answersLength === 0) {
+    return <div className="qa-div-answers" data-testid="qa-answers-div">Be the first to answer this question!</div>;
+  }
+
+  // if there are two or less answers, don't show any buttons
+  if (answersLength <= 2) {
+    return <div data-testid="qa-answers-div" data-testid="qa-div-card-answers">
+      {answersToShow.map((answer, index) =>
+        <div className="qa-answers-external" key={index}>
+          <div className="qa-div-answers" ><b>A:</b> {answer.body}</div>
+          <p className="qa-footer"> by {answer.answerer_name}, {format((new Date(answer.date)), "MMMM dd, yyyy")}
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallHelpful(answer.id)}>Helpful? ({answer.helpfulness})</span>
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallReport(answer.id)}>Report</span>
+          </p>
+        </div>
+      )}
+    </div>;
+  }
+
+  // if there are more answers than those displayed, show "load more" button
+  if (answersLength >= (answersToShowLength - 1)) {
+    return <div data-testid="qa-answers-div" data-testid="qa-div-card-answers">
+      {answersToShow.map((answer, index) =>
+        <div className="qa-answers-external" key={index}>
+          <div className="qa-div-answers" ><b>A:</b> {answer.body}</div>
+          <p className="qa-footer"> by {answer.answerer_name}, {format((new Date(answer.date)), "MMMM dd, yyyy")}
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallHelpful(answer.id)}>Helpful? ({answer.helpfulness})</span>
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallReport(answer.id)}>Report</span>
+          </p>
+        </div>
+      )}
+      <button data-testid="qa-load-more" className="qa-load-more" onClick={() => {
+        setAnswersToShowLength(answersToShowLength + 2);
+        setAnswersToShow(answers.slice(0, answersToShowLength));
+      }}><b><span className="qa-load">[LOAD MORE ANSWERS]</span></b></button>
+    </div>;
+  }
+
+  // if answers array is at max, show "collapse button"
+  if (answersLength <= (answersToShowLength - 1)) {
+    return <div data-testid="qa-answers-div" data-testid="qa-div-card-answers">
+      {answersToShow.map((answer, index) =>
+        <div className="qa-answers-external" key={index}>
+          <div className="qa-div-answers" ><b>A:</b> {answer.body}</div>
+          <p className="qa-footer"> by {answer.answerer_name}, {format((new Date(answer.date)), "MMMM dd, yyyy")}
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallHelpful(answer.id)}>Helpful? ({answer.helpfulness})</span>
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallReport(answer.id)}>Report</span>
+          </p>
+        </div>
+      )}
+      <button data-testid="qa-load-more" className="qa-load-more" onClick={() => {
+        setAnswersToShowLength(2);
+        setAnswersToShow(answers.slice(0, 2));
+      }}><b><span className="qa-load">[COLLAPSE ANSWERS]</span></b></button>
+    </div>;
   }
 
   return (
-    <div data-testid="qa-answers-div">
-      <div data-testid="qa-div-card-answers">
-        {answersToShow.map((answer, index) =>
-          <div className="qa-answers-external" key={index}>
-            <div className="qa-div-answers" ><b>A:</b> {answer.body}</div>
-            <p className="qa-footer"> by {answer.answerer_name}, {format((new Date(answer.date)), "MMMM dd, yyyy")}
-              <span className="qa-divider">|</span>
-              <span onClick={() => APICall(answer.id)}>Helpful? ({answer.helpfulness})</span>
-              <span className="qa-divider">|</span>
-              <span onClick={() => APICallReport(answer.id)}>Report</span>
-            </p>
-          </div>
-        )}
-        <button data-testid="qa-load-more" className="qa-load-more" onClick={() => {
-          setAnswersToShowLength(answersToShowLength + 2);
-          setAnswersToShow(answers.slice(0, answersToShowLength));
-        }}><b><span className="qa-load">[LOAD MORE ANSWERS]</span></b></button>
-      </div>
+    <div data-testid="qa-answers-div" data-testid="qa-div-card-answers">
+      {answersToShow.map((answer, index) =>
+        <div className="qa-answers-external" key={index}>
+          <div className="qa-div-answers" ><b>A:</b> {answer.body}</div>
+          <p className="qa-footer"> by {answer.answerer_name}, {format((new Date(answer.date)), "MMMM dd, yyyy")}
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallHelpful(answer.id)}>Helpful? ({answer.helpfulness})</span>
+            <span className="qa-divider">|</span>
+            <span onClick={() => APICallReport(answer.id)}>Report</span>
+          </p>
+        </div>
+      )}
+      <button data-testid="qa-load-more" className="qa-load-more" onClick={() => {
+        setAnswersToShowLength(answersToShowLength + 2);
+        setAnswersToShow(answers.slice(0, answersToShowLength));
+      }}><b><span className="qa-load">[LOAD MORE ANSWERS]</span></b></button>
     </div>
   );
 };
 
 export default QACardAnswers;
-
-
