4515fcb81b17a7c5b1e741b74ce32605d86150b5
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-06-30 23:24:06 -0700

    Adds form validation to add question modal, adds placeholders

--- client/src/components/QA/QAAddQuestionModal.jsx
+++ client/src/components/QA/QAAddQuestionModal.jsx
@@ -10,10 +10,30 @@ const QAAddQuestionModal = ({ productName, currentProductId }) => {
   // sets up an object with responses, will send this to POST requests
   const [modalInfo, setModalInfo] = useState({question: '', nickname: '', email: ''});
 
+
   const APICall = (question, nickname, email) => {
-    axios.post('/qa/questions', ({product_id: currentProductId, body: question, name: nickname, email: email}))
-      .then(info => info)
-      .catch(err => err);
+    let message = '';
+    if (question === '') {
+      message += ' Please include a question. \n ';
+    }
+    if (nickname === '') {
+      message += ' Please include a nickname. \n ';
+    }
+    if (email === '') {
+      message += ' Please include an email. \n ';
+    }
+    if (email !== '' && email.includes('@') === false) {
+      message += ' Please include a valid email. \n ';
+    }
+
+    if (message !== '') {
+      alert(`You must enter the following: \n ${message}`);
+      document.getElementById('qa-modal-add-question').style.display = 'block';
+    } else if (message === '') {
+      axios.post('/qa/questions', ({product_id: currentProductId, body: question, name: nickname, email: email}))
+        .then(info => info)
+        .catch(err => err);
+    }
   };
 
   return (
@@ -30,11 +50,11 @@ const QAAddQuestionModal = ({ productName, currentProductId }) => {
           <h3>About the {productName.name}</h3>
           <form className="add-answer-form">
             <label className="add-form-question">Question*</label>
-            <textarea rows="10" maxLength="1000" required onChange={() => setQuestion(event.target.value)} /><p></p>
+            <textarea rows="10" maxLength="1000" placeholder="Why did you like the product or not?" required onChange={() => setQuestion(event.target.value)} /><p></p>
             <label className="add-form-username">Nickame*</label>
             <input maxLength="60" placeholder="Example: jackson11!" required onChange={() => setNickname(event.target.value)}/><p className="qa-form-small">For privacy reasons, do not use your full name or email address</p>
             <label className="add-form-email">Email*</label>
-            <input maxLength="60" required onChange={() => setEmail(event.target.value)}/><p className="qa-form-small">For authentication reasons, you will not be emailed</p>
+            <input maxLength="60" placeholder="Example: jack@email.com" required onChange={() => setEmail(event.target.value)}/><p className="qa-form-small">For authentication reasons, you will not be emailed</p>
             <button className="qa-add-form-submit" onClick={() => {
               event.preventDefault();
               document.getElementById('qa-modal-add-question').style.display = 'none';
